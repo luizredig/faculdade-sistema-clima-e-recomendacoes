a0a346e6322c063b9b406193d4eae7e94f2e6ae5
redig <luizredig@gmail.com>
2025-04-16 21:38:03 -0300

    feat: adiciona redis

--- api-a/app.js
+++ api-a/app.js
@@ -3,25 +3,43 @@ require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const axios = require("axios");
 const app = express();
+const redis = require("redis");
+
+let redis_client;
+let redis_data;
 
 const PORT = process.env.PORT;
 const API_B_URL = process.env.API_B_URL;
 
 async function fetchData(citySlug) {
   try {
-    const response = await axios.get(`${API_B_URL}/${citySlug}`);
-    const { city, temp, unit } = response.data;
+    let data;
 
-    let recommendation;
+    if (!redis_data) {
+      const response = await axios.get(`${API_B_URL}/${citySlug}`);
 
-    if (temp > 30) {
-      recommendation =
-        "Está muito quente! Mantenha-se hidratado e use protetor solar.";
-    } else if (temp > 15) {
-      recommendation = "Clima agradável. Aproveite seu dia!";
-    } else {
-      recommendation = "Está frio! Vista um casaco.";
+       client.set(cacheKey, JSON.stringify(response.data), {
+         EX: 60,
+       });
+      
+      data = response.data;
+      redis_data = response.data;
+
+      const { city, temp, unit } = response.data;
+
+      let recommendation;
+
+      if (temp > 30) {
+        recommendation =
+          "Está muito quente! Mantenha-se hidratado e use protetor solar.";
+      } else if (temp > 15) {
+        recommendation = "Clima agradável. Aproveite seu dia!";
+      } else {
+        recommendation = "Está frio! Vista um casaco.";
+      }
     }
+
+    const { city, temp, unit } = data;
 
     return {
       city,
@@ -36,10 +54,25 @@ async function fetchData(citySlug) {
 }
 
 app.get("/recommendation/:city", async (req, res) => {
-  const citySlug = req.params.city;
+  if (!redis_client) {
+    redis_client = redis.createClient({
+      host: "redis",
+      port: 6379,
+    });
+
+    redis_client.on("error", (err) => {
+      console.error("Redis error:", err);
+    });
+    await redis_client.connect();
+  }
+
+  const cacheKey = req.params.city;
+  let data = await redis_client.get(cacheKey);
+
+  redis_data = data !== null ? JSON.parse(data) : null;
 
   try {
-    const result = await fetchData(citySlug);
+    const result = await fetchData(cacheKey);
     res.json(result);
   } catch (err) {
     res.status(500).json({ error: err.message });
